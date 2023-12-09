a2a35a550961e257c2c9d70819e0e70b902e7a3c
AmrAbaza29 <134836859+AmrAbaza29@users.noreply.github.com>
2023-12-09 21:27:46 +0200

    ApiResponseDTO

---
+++ Vezeeta.System.BL/Managers/DTOs/ApiResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vezeeta.System.BL.Managers.DTOs
+{
+    public class ApiResponseDTO<T>
+    {
+        public bool Success { get; set; }
+        public List<T> Data { get; set; } = new List<T>();
+        public string Message { get; set; }
+    }
+}
